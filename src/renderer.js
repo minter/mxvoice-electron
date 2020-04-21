f58bc2e3f07ddce3205052a068e6ddc215504d35
H. Wade Minter <minter@lunenburg.org>
2020-04-20 22:34:19 -0400

    Checkpoint - category select box, can search by combinations of category and term

--- src/renderer.js
+++ src/renderer.js
@@ -1,13 +1,42 @@
 var db = new sqlite3.Database(path.join(homedir, 'mrvoice.db'));
 var sound;
 
+function populateCategorySelect(){
+  console.log("Populating categories");
+  db.each("SELECT * FROM categories ORDER BY description ASC", [], function(err, row) {
+  if (err) {
+    throw err;
+  }
+    $('#category_select').append('<option value="' + row.code + '">' + row.description + '</option>');
+    console.log('Found ' + row.code + ' as ' + row.description);
+  });
+
+}
+
 function searchData(){
   db.serialize(function() {
+    var category = $( "#category_select" ).val();
+    console.log('Using category ' + category);
     var value = document.getElementById('omni_search').value
     console.log('Called with value ' + value);
     $('#search_results tbody').find("tr").remove();
     var search_term = '%' + value + '%';
-    db.each("SELECT * from mrvoice WHERE info LIKE ? OR title LIKE ? OR artist like ?", [search_term, search_term, search_term], function(err, row) {
+    var query_params = [];
+    var query_segments = [];
+    var query_string = '';
+    if (category != '*') {
+      query_segments.push('category = ?');
+      query_params.push(category);
+    }
+    if (value != '') {
+      query_segments.push('(info LIKE ? OR title LIKE ? OR artist like ?)');
+      query_params.push(search_term, search_term, search_term);
+    }
+    if (query_segments.length != 0) {
+      query_string = " WHERE " + query_segments.join(' AND ');
+    }
+    console.log("Query string is " + query_string);
+    db.each("SELECT * from mrvoice" + query_string, query_params, function(err, row) {
     if (err) {
       throw err;
     }
@@ -18,18 +47,7 @@ function searchData(){
 }
 
 function playSelected(){
-  //$('#selected_row').attr('songid');
-  //$('#selected_row').removeAttr('id');
-
-  // $('#myTable').on('click', '.clickable-row', function(event) {
-  //   if($(this).hasClass('active')){
-  //     $(this).removeClass('active');
-  //   } else {
-  //     $(this).addClass('active').siblings().removeClass('active');
-  //   }
-  // });
   var song_id = $('#selected_row').attr('songid');
-  // var filename;
   console.log('Got song ID ' + song_id);
 
   if (song_id) {
