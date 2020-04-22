2cd053d744b426cf2cb63f685d96e7d8935362e2
Andrew Berkowitz <andrew@teamsnap.com>
2020-04-21 17:00:06 -0700

    Enable hot reload of HTML/CSS
    Saving a change to .html or .css files reloads the window.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+// Enable live reload
+
+require("electron-reload")(__dirname);
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
