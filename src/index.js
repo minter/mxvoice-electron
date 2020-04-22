96968ed1ae54da9bab937bc8dcf48076f11091f4
Andrew Berkowitz <andrew@teamsnap.com>
2020-04-22 10:58:53 -0700

    Oops, hide devtools on launch

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   mainWindow.$ = mainWindow.jQuery = require('jquery');
   // mainWindow.Bootstrap = require('bootstrap');
