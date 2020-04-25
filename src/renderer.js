70de1a4b5abb2c7ec83ad9c541f82118482bb0f5
H. Wade Minter <minter@lunenburg.org>
2020-04-25 18:44:34 -0400

    Nobody likes to see 'null' in the output

--- src/renderer.js
+++ src/renderer.js
@@ -84,7 +84,7 @@ function searchData(){
       throw err;
     }
         //console.log('Found ' + row.title + ' by ' + row.artist);
-        $("#search_results").append(`<tr draggable='true' ondragstart='songDrag(event)' class='song unselectable' songid='${row.id}'><td>${categories[row.category]}</td><td>${row.info}</td><td>${row.title}</td><td>${row.artist}</td><td>${row.time}</td></tr>`);
+        $("#search_results").append(`<tr draggable='true' ondragstart='songDrag(event)' class='song unselectable' songid='${row.id}'><td>${categories[row.category]}</td><td>${row.info || ''}</td><td>${row.title || ''}</td><td>${row.artist || ''}</td><td>${row.time}</td></tr>`);
 
     });
   });
